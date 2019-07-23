38253c1f873a96286d77c1889794626f2b9d79b2
khenryjoe <joseph.ketterer@gmail.com>
2019-07-23 11:02:11 +0100

    added p

---
+++ src/Palette.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+
+
+class Palette extends Component {
+
+
+    render(){
+        return(
+            <div className="Palette">
+                {/* navbar */}
+                <div className="Pallette-colors">
+                  {/* colors */}
+                </div>
+                {/* footer */}
+            </div>
+        );
+
+    }
+}
+
+export default Palette;
